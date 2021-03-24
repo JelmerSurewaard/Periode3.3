805240fc61faf5d1cf2bd214be9444738eca5db3
Jelmer <Jelmer.surewaard@gmail.com>
2021-03-24 14:58:17 +0100

    [CHANGE] ip

--- backend.js
+++ backend.js
@@ -2,7 +2,7 @@ var mqtt = require('mqtt')
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://192.168.178.43:27017'
 const dbName = 'SmartmeterDb'
 const client = new MongoClient(url)
 var mqtt = mqtt.connect('mqtt://test.mosquitto.org')
