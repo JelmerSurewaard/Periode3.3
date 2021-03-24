60197678b1adc90f471eaa5e5f7e7343d504a7c5
Jelmer <Jelmer.surewaard@gmail.com>
2021-03-24 10:46:32 +0100

    [CHANGE] Removed everything I don't need
    sorry Ties en Chiem

--- IWSN/angular-smartmeter-frontend/src/app/Menu/menu.ts
+++
@@ -1,5 +0,0 @@
-export class List {
-    id: number | undefined;
-    text: string | undefined;
-    icon: string | undefined;
-}
--- IWSN/angular-smartmeter-frontend/src/app/Models/SmartMeterData.ts
+++
@@ -1,6 +0,0 @@
-export interface SmartmeterData {
-    MQTT_USER : string;
-    Time : number;
-    electricity_delivered_to_client_tariff_1 : string;
-    Actual_electricity_power_delivered_plus : string;
-}
